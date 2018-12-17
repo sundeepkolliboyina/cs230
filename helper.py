a4c821b5ddf179f64520a7114442dd5c27dec29b
Ubuntu <ubuntu@ip-172-31-19-212.us-west-2.compute.internal>
2018-12-17 05:16:20 +0000

    cs230 submission

---
+++ helper.py
@@ -0,0 +1,243 @@
+import re
+import random
+import os.path
+import scipy.misc
+import shutil
+import zipfile
+import time
+import tensorflow as tf
+from glob import glob
+from urllib.request import urlretrieve
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+plt.switch_backend('agg')
+from sklearn.metrics import confusion_matrix
+import numpy as np
+from skimage.color import rgba2rgb
+from skimage import data
+
+
+class DLProgress(tqdm):
+    last_block = 0
+
+    def hook(self, block_num=1, block_size=1, total_size=None):
+        self.total = total_size
+        self.update((block_num - self.last_block) * block_size)
+        self.last_block = block_num
+
+
+def maybe_download_pretrained_vgg(data_dir):
+    """
+    Download and extract pretrained vgg model if it doesn't exist
+    :param data_dir: Directory to download the model to
+    """
+    vgg_filename = 'vgg.zip'
+    vgg_path = os.path.join(data_dir, 'vgg')
+    vgg_files = [
+        os.path.join(vgg_path, 'variables/variables.data-00000-of-00001'),
+        os.path.join(vgg_path, 'variables/variables.index'),
+        os.path.join(vgg_path, 'saved_model.pb')]
+
+    missing_vgg_files = [vgg_file for vgg_file in vgg_files if not os.path.exists(vgg_file)]
+    if missing_vgg_files:
+        # Clean vgg dir
+        if os.path.exists(vgg_path):
+            shutil.rmtree(vgg_path)
+        os.makedirs(vgg_path)
+
+        # Download vgg
+        print('Downloading pre-trained vgg model...')
+        with DLProgress(unit='B', unit_scale=True, miniters=1) as pbar:
+            urlretrieve(
+                'https://s3-us-west-1.amazonaws.com/udacity-selfdrivingcar/vgg.zip',
+                os.path.join(vgg_path, vgg_filename),
+                pbar.hook)
+
+        # Extract vgg
+        print('Extracting model...')
+        zip_ref = zipfile.ZipFile(os.path.join(vgg_path, vgg_filename), 'r')
+        zip_ref.extractall(data_dir)
+        zip_ref.close()
+
+        # Remove zip file to save space
+        os.remove(os.path.join(vgg_path, vgg_filename))
+
+
+def compute_iou(y_pred, y_true):
+    # ytrue, ypred is a flatten vector
+    y_pred = y_pred.flatten()
+    y_true = y_true.flatten()
+    current = confusion_matrix(y_true, y_pred, labels = [0, 1])
+    # compute mean iou
+    intersection = np.diag(current)
+    ground_truth_set = current.sum(axis=1)
+    predicted_set = current.sum(axis=0)
+    union = ground_truth_set + predicted_set - intersection
+    IoU = intersection / union.astype(np.float32)
+    return np.mean(IoU)
+
+# def gen_iou(pred,gt_image,num_classes): 
+#    class_iou=np.zeros(num_classes)
+#    class_weight=np.zeros(num_classes)
+#    for i in range(num_classes): 
+#        intersection = np.float32(np.sum((pred==gt_image)*(gt_image==i)))
+#        union = np.sum(gt_image==i)+np.sum(pred==i)-intersection 
+#        if union > 0:
+#            class_iou[i]=intersection/union
+#            class_weight[i]=union
+#    #SK print("Mean classes IoU : " + str(np.mean(class_iou))) 
+#    #SK print("Image predicition accuracy : " + str(np.float32(np.sum(pred == gt_image)) / gt_image.size))
+#
+#    return class_iou, class_weight
+
+def gen_batch_function(data_folder, image_shape):
+    """
+    Generate function to create batches of training data
+    :param data_folder: Path to folder that contains all the datasets
+    :param image_shape: Tuple - Shape of image
+    :return:
+    """
+    def get_batches_fn(batch_size):
+        """
+        Create batches of training data
+        :param batch_size: Batch Size
+        :return: Batches of training data
+        """
+        image_paths = glob(os.path.join(data_folder, 'image_2', '*resize.png'))
+        label_paths = {
+            # re.sub(r'_(drivable_color)_', '_', os.path.basename(path)): path
+            re.sub(r'_(drivable_color)', '', os.path.basename(path)): path
+            for path in glob(os.path.join(data_folder, 'gt_image_2', '*resize.png'))}
+        # background_color = np.array([255, 0, 0])
+        background_color = np.array([0, 0, 0])
+
+        random.shuffle(image_paths)
+        for batch_i in range(0, len(image_paths), batch_size):
+            images = []
+            gt_images = []
+            for image_file in image_paths[batch_i:batch_i+batch_size]:
+                gt_image_file = label_paths[os.path.basename(image_file)]
+
+
+                image = scipy.misc.imresize(scipy.misc.imread(image_file), image_shape)
+                gt_image = scipy.misc.imresize(scipy.misc.imread(gt_image_file), image_shape)
+
+                gt_bg = np.all(gt_image == background_color, axis=2)
+                gt_bg = gt_bg.reshape(*gt_bg.shape, 1)
+                gt_image = np.concatenate((gt_bg, np.invert(gt_bg)), axis=2)
+
+                images.append(image)
+                gt_images.append(gt_image)
+
+            yield np.array(images), np.array(gt_images)
+    return get_batches_fn
+
+
+def gen_test_output(sess, logits, keep_prob, image_pl, data_folder, image_shape):
+    """
+    Generate test output using the test images
+    :param sess: TF session
+    :param logits: TF Tensor for the logits
+    :param keep_prob: TF Placeholder for the dropout keep robability
+    :param image_pl: TF Placeholder for the image placeholder
+    :param data_folder: Path to the folder that contains the datasets
+    :param image_shape: Tuple - Shape of image
+    :return: Output for for each test image
+    """
+    for image_file in glob(os.path.join(data_folder, 'image_2', '*.jpg')):
+        image = scipy.misc.imresize(scipy.misc.imread(image_file), image_shape)
+
+        im_softmax = sess.run(
+            [tf.nn.softmax(logits)],
+            {keep_prob: 1.0, image_pl: [image]})
+        im_softmax = im_softmax[0][:, 1].reshape(image_shape[0], image_shape[1])
+        segmentation = (im_softmax > 0.5).reshape(image_shape[0], image_shape[1], 1)
+        mask = np.dot(segmentation, np.array([[0, 255, 0, 127]]))
+        mask = scipy.misc.toimage(mask, mode="RGBA")
+        street_im = scipy.misc.toimage(image)
+        street_im.paste(mask, box=None, mask=mask)
+
+        yield os.path.basename(image_file), np.array(street_im)
+
+
+def save_inference_samples(runs_dir, data_dir, sess, image_shape, logits, keep_prob, input_image, epoch_nr):
+    # Make folder for current run
+    subfolder = "epoch" + str(epoch_nr)
+    output_dir = os.path.join(runs_dir, subfolder)
+    print("Creating directory", output_dir)
+    if os.path.exists(output_dir):
+        shutil.rmtree(output_dir)
+    os.makedirs(output_dir)
+
+    # Run NN on test images and save them to HD
+    print('Saving test images to: {}'.format(output_dir))
+    image_outputs = gen_test_output(
+        sess, logits, keep_prob, input_image, os.path.join(data_dir, 'data_road/testing'), image_shape)
+    for name, image in image_outputs:
+        scipy.misc.imsave(os.path.join(output_dir, name), image)
+
+def gen_val_output(sess, logits, keep_prob, image_pl, data_folder, image_shape):
+    """
+    Generate test output using the test images
+    :param sess: TF session
+    :param logits: TF Tensor for the logits
+    :param keep_prob: TF Placeholder for the dropout keep robability
+    :param image_pl: TF Placeholder for the image placeholder
+    :param data_folder: Path to the folder that contains the datasets
+    :param image_shape: Tuple - Shape of image
+    :return: Output for for each test image
+    """
+    for image_file in glob(os.path.join(data_folder, 'image_2', '*resize*.png')):
+        image = scipy.misc.imresize(scipy.misc.imread(image_file), image_shape)
+
+        im_softmax = sess.run(
+            [tf.nn.softmax(logits)],
+            {keep_prob: 1.0, image_pl: [image]})
+        im_softmax = im_softmax[0][:, 1].reshape(image_shape[0], image_shape[1])
+        segmentation = (im_softmax > 0.5).reshape(image_shape[0], image_shape[1], 1)
+        mask = np.dot(segmentation, np.array([[0, 255, 0, 127]]))
+        mask = scipy.misc.toimage(mask, mode="RGBA")
+        street_im = scipy.misc.toimage(image)
+        street_im.paste(mask, box=None, mask=mask)
+
+        yield os.path.basename(image_file), np.array(street_im)
+
+
+def save_val_samples(val_dir, data_dir, sess, image_shape, logits, keep_prob, input_image, epoch_nr):
+    # Make folder for current run
+    subfolder = "epoch" + str(epoch_nr)
+    output_dir = os.path.join(val_dir, subfolder)
+    print("Creating directory", output_dir)
+    if os.path.exists(output_dir):
+        shutil.rmtree(output_dir)
+    os.makedirs(output_dir)
+
+    # Run NN on test images and save them to HD
+    print('Saving val images to: {}'.format(output_dir))
+    print("#--------------------------------------------------------")
+    image_outputs = gen_val_output(
+        sess, logits, keep_prob, input_image, os.path.join(data_dir, 'data_road/validation'), image_shape)
+#    for name, image in image_outputs:
+#        scipy.misc.imsave(os.path.join(output_dir, name), image)
+#        val_image = scipy.misc.imread((os.path.abspath(output_dir)) + "/" + str(name))
+#        val_gt_image  = scipy.misc.imread('data/data_road/validation/gt_image_2/' + str(name))
+#        val_gt_image  = rgba2rgb(val_gt_image)
+#        print(val_image.shape)
+#        print(val_gt_image.shape)
+#        IoU = compute_iou(val_image, val_gt_image)
+#        print("IoU : { }".format(IoU))
+
+def plot_loss(plot_dir, loss, folder_name):
+    _, axes = plt.subplots()
+    plt.plot(range(0, len(loss)), loss)
+    plt.title('Cross-entropy loss')
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.grid()
+    if os.path.exists(plot_dir):
+        shutil.rmtree(plot_dir)
+    os.makedirs(plot_dir)
+
+    output_file = os.path.join(plot_dir, folder_name + ".png")
+    plt.savefig(output_file)
+
